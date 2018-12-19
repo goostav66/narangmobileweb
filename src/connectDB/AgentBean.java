e47be6a388fc557ef0749f0b0a8dfe06233b0594
goostav66 <34259724+goostav66@users.noreply.github.com>
2018-12-19 12:09:47 +0900

    Create AgentBean.java

---
+++ src/connectDB/AgentBean.java
@@ -0,0 +1,110 @@
+package connectDB;
+
+public class AgentBean {
+	private int agent_idx;
+	private String branch_code;
+	private String agent_code;
+	private String agent_id;
+	private String agent_name;
+	private int agent_rw;
+	private String agent_ceo_name;
+	private String agent_ceo_phone;
+	private String agent_manager_name;
+	private String agent_manager_phone;
+	private String agent_fax_number;
+	private String agent_rgst_date;
+	private String agent_addr;
+	private String agent_bank_name;
+	private String agent_bank_num;
+	public int getAgent_idx() {
+		return agent_idx;
+	}
+	public void setAgent_idx(int agent_idx) {
+		this.agent_idx = agent_idx;
+	}
+	public String getBranch_code() {
+		return branch_code;
+	}
+	public void setBranch_code(String branch_code) {
+		this.branch_code = branch_code;
+	}
+	public String getAgent_code() {
+		return agent_code;
+	}
+	public void setAgent_code(String agent_code) {
+		this.agent_code = agent_code;
+	}
+	public String getAgent_id() {
+		return agent_id;
+	}
+	public void setAgent_id(String agent_id) {
+		this.agent_id = agent_id;
+	}
+	public String getAgent_name() {
+		return agent_name;
+	}
+	public void setAgent_name(String agent_name) {
+		this.agent_name = agent_name;
+	}
+	public int getAgent_rw() {
+		return agent_rw;
+	}
+	public void setAgent_rw(int agent_rw) {
+		this.agent_rw = agent_rw;
+	}
+	public String getAgent_ceo_name() {
+		return agent_ceo_name;
+	}
+	public void setAgent_ceo_name(String agent_ceo_name) {
+		this.agent_ceo_name = agent_ceo_name;
+	}
+	public String getAgent_ceo_phone() {
+		return agent_ceo_phone;
+	}
+	public void setAgent_ceo_phone(String agent_ceo_phone) {
+		this.agent_ceo_phone = agent_ceo_phone;
+	}
+	public String getAgent_manager_name() {
+		return agent_manager_name;
+	}
+	public void setAgent_manager_name(String agent_manager_name) {
+		this.agent_manager_name = agent_manager_name;
+	}
+	public String getAgent_manager_phone() {
+		return agent_manager_phone;
+	}
+	public void setAgent_manager_phone(String agent_manager_phone) {
+		this.agent_manager_phone = agent_manager_phone;
+	}
+	public String getAgent_fax_number() {
+		return agent_fax_number;
+	}
+	public void setAgent_fax_number(String agent_fax_number) {
+		this.agent_fax_number = agent_fax_number;
+	}
+	public String getAgent_rgst_date() {
+		return agent_rgst_date;
+	}
+	public void setAgent_rgst_date(String agent_rgst_date) {
+		this.agent_rgst_date = agent_rgst_date;
+	}
+	public String getAgent_addr() {
+		return agent_addr;
+	}
+	public void setAgent_addr(String agent_addr) {
+		this.agent_addr = agent_addr;
+	}
+	public String getAgent_bank_name() {
+		return agent_bank_name;
+	}
+	public void setAgent_bank_name(String agent_bank_name) {
+		this.agent_bank_name = agent_bank_name;
+	}
+	public String getAgent_bank_num() {
+		return agent_bank_num;
+	}
+	public void setAgent_bank_num(String agent_bank_num) {
+		this.agent_bank_num = agent_bank_num;
+	}
+	
+}
